7df2a2173b44c2a3122cb85fdda84ab7ace746bb
biswash <117192458+Dark-gem@users.noreply.github.com>
2024-12-04 00:43:55 +0545

    try2.py

--- try2.py
+++ try2.py
@@ -32,7 +32,7 @@ db = SQLAlchemy(app)
 # Logging configuration
 logging.basicConfig(level=logging.DEBUG)
 
-# Models
+# Model
 class Employee(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100))
